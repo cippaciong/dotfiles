01d2367c3849a690b27760338f24621451e88ce6
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2024-02-05 22:48:25 +0100

    Add custom nvim config for nvchad

---
+++ config/nvim/lua/custom/configs/treesitter.lua
@@ -0,0 +1,18 @@
+local options = {
+  ensure_installed = {
+    "css",
+    "go",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "ruby",
+    "typescript",
+    "tsx",
+    "vim",
+    "yaml",
+  },
+}
+
+return options
