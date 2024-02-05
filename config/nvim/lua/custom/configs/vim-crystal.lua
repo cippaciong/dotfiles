2df24d0620cc0618706c55cc64fe75352f75172f
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2024-02-06 00:09:41 +0100

    Add crystal support to nvim

---
+++ config/nvim/lua/custom/configs/vim-crystal.lua
@@ -0,0 +1,1 @@
+vim.g.crystal_auto_format = 1
--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -1,8 +1,16 @@
 local plugins = {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function ()
+    opts = function()
       return require "custom.configs.treesitter"
+    end,
+  },
+
+  {
+    "vim-crystal/vim-crystal",
+    ft = "crystal",
+    config = function()
+      require "custom.configs.vim-crystal"
     end,
   },
 }
