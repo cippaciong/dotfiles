3a8b4dfb01e52d95f0b854da4340fb9aaf7ffca0
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2024-11-02 18:19:56 +0100

    Add nvim-cmp for autocompletion

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -227,6 +227,99 @@ require('lazy').setup({
       end,
     },
 
+    -- autocompletion
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "onsails/lspkind-nvim",
+      },
+      config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
+
+        cmp.event:on("confirm_done")
+
+        luasnip.config.setup {}
+
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
+        require('cmp').setup({
+          snippet = {
+              expand = function(args)
+                -- REQUIRED - you must specify a snippet engin
+                luasnip.lsp_expand(args.body)
+              end,
+          },
+          formatting = {
+            format = lspkind.cmp_format {
+              with_text = true,
+              menu = {
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[Lua]",
+              },
+            },
+          },
+          mapping = cmp.mapping.preset.insert {
+            -- Mappings to nagivigate through completions
+            ['<C-n>'] = cmp.mapping.select_next_item(),
+            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<CR>'] = cmp.mapping.confirm { select = true },
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+          },
+          -- Don't auto select item
+          preselect = cmp.PreselectMode.None,
+          window = {
+            documentation = cmp.config.window.bordered(),
+          },
+          view = {
+            entries = {
+              name = "custom",
+              selection_order = "near_cursor",
+            },
+          },
+          confirm_opts = {
+            behavior = cmp.ConfirmBehavior.Insert,
+          },
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = "luasnip", keyword_length = 2},
+            { name = "buffer", keyword_length = 5},
+          },
+        })
+      end,
+    },
+
   },
 
   -- Colorscheme that will be used when installing plugins. That happens before startup,
