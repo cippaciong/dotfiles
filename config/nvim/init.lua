8a56e8faa8423961ebc042e1d01393cc2ebe0f19
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2024-11-03 21:15:06 +0100

    Add lua LSP configuration for nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -247,7 +247,38 @@ require('lazy').setup({
 
         -- Ruby (https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ruby_lsp)
         require'lspconfig'.ruby_lsp.setup{}
-      end,
+
+        -- Lua LSP configuration to work with neovim files
+        -- (https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls)
+        require'lspconfig'.lua_ls.setup {
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+                return
+              end
+            end
+
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME
+                }
+              }
+            })
+          end,
+          settings = {
+            Lua = {}
+          }
+        }
+      end
     },
 
     -- autocompletion
