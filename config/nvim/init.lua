f3a3f450a33da994375dd62e9106e927a1181437
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2026-09-08 17:31:22 +0200

    Add Markdown preview shortcut

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -380,6 +380,9 @@ require('lazy').setup({
       "iamcco/markdown-preview.nvim",
       cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
       ft = { "markdown" },
+      keys = {
+        { "<leader>m", "<cmd>MarkdownPreview<CR>", desc = "Preview Markdown" },
+      },
       build = function()
         require("lazy").load({ plugins = { "markdown-preview.nvim" } })
         vim.fn["mkdp#util#install"]()
